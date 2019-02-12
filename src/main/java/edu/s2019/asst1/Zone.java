5382dcb316053ce9af4135e01a83819100057968
Abhijeet Mishra <aviaw@localhost.localdomain>
2019-02-11 19:19:38 -0500

    Add conditions regarding the wall adjacency in case the current defined conditions are met, but they still dont share a wall.

--- src/main/java/edu/s2019/asst1/Zone.java
+++ src/main/java/edu/s2019/asst1/Zone.java
@@ -45,32 +45,64 @@ public class Zone implements ZoneInterface {
         return  new Zone(newHeight,newWidth,newBasePoint);
     }
 
-    public void mergeZone(Zone zone)
+    public boolean mergeZone(Zone zone)
     {
-        //todo
+        //Add conditions regarding the wall adjacency in case the current defined conditions are met, but they still dont share a wall.
+        if(zone.basePoint.x != basePoint.x && zone.basePoint.y == basePoint.y && height == zone.height) {
+            if (zone.basePoint.x <= basePoint.x) {
+                basePoint.x = zone.basePoint.x;
+
+            }
+            this.widht+=zone.widht;
+        }
+        else if(zone.basePoint.y != basePoint.y && widht == zone.widht && zone.basePoint.x == basePoint.x) {
+            if (zone.basePoint.y <= basePoint.y) {
+                basePoint.y = zone.basePoint.y;
+
+            }
+            this.height += zone.height;
+        }
+        else{
+            return false;
+        }
+
+        zone.basePoint = new Point(-1,-1);
+        zone.height = -1;
+        zone.widht = -1;
+        return true;
     }
 
     public void printZone()
     {
-        System.out.println("\n\n\n****************\n");
-        System.out.println("The Zone's 4 corners are (in X Y format) widht and height are "+widht+" "+height);
-        System.out.println("Bottom Left --> "+this.basePoint.x+" "+this.basePoint.y);
-        System.out.println("Upper Left --> "+this.basePoint.x+" "+(this.basePoint.y+height));
-        System.out.println("Upper Right --> "+(this.basePoint.x+widht)+" "+(this.basePoint.y+height));
-        System.out.println("Bottom Right --> "+(this.basePoint.x+widht)+" "+this.basePoint.y);
+        if(height == -1 )
+        {
+            System.out.println("The zone doesnt exist or hasent been initialised");
+        }
+        else {
+            System.out.println("\n\n****************\n");
+            System.out.println("The Zone's 4 corners are (in X Y format) widht and height are " + widht + " " + height);
+            System.out.println("Bottom Left --> " + this.basePoint.x + " " + this.basePoint.y);
+            System.out.println("Upper Left --> " + this.basePoint.x + " " + (this.basePoint.y + height));
+            System.out.println("Upper Right --> " + (this.basePoint.x + widht) + " " + (this.basePoint.y + height));
+            System.out.println("Bottom Right --> " + (this.basePoint.x + widht) + " " + this.basePoint.y);
+            System.out.println("************************\n");
+        }
     }
 
     public static void main(String[] args){
-        // basic Checks
 
         Zone zone = new Zone();
-        zone.printZone();
         Zone newZone = zone.splitZone();
+        Zone tempZone = zone.splitZone();
+        tempZone = zone.splitZone();
+        newZone = tempZone.splitZone();
+        newZone.printZone();
+        zone.printZone();
+        tempZone.printZone();
+        System.out.println(newZone.mergeZone(zone));
+        System.out.println(newZone.mergeZone(tempZone));
         zone.printZone();
         newZone.printZone();
-        zone.splitZone().printZone();
-        zone.printZone();
-
     }
 
 
--- src/main/java/edu/s2019/asst1/implement/ZoneInterface.java
+++ src/main/java/edu/s2019/asst1/implement/ZoneInterface.java
@@ -1,15 +1,11 @@
 package edu.s2019.asst1.implement;
 
-import java.awt.*;
-import java.util.ArrayList;
-
 import edu.s2019.asst1.Zone;
 
 public interface ZoneInterface{
 
 
     Zone splitZone();
-    void mergeZone(Zone zone);
+    boolean mergeZone(Zone zone);
     void printZone();
-
 }
